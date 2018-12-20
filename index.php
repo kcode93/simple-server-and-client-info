0525e742f40c3b7a41d8b2020d2a45128063698c
Kenneth Garcia <kennethgarcia12@hotmail.com>
2018-12-20 10:29:02 -0800

    added the server info importing them from the server-info.php

--- index.php
+++ index.php
@@ -17,8 +17,19 @@
     <title>The Lake</title>
   </head><!--end of head-->
   <body>
-
-
+    <div class="container">
+      <h1>Server & File Info</h1>
+      <?php if($server): ?>
+        <ul class="list-group">
+          <?php foreach ($server as $key => $value):?>
+            <li class="list-group-item">
+              <strong><?php echo $key; ?></strong>
+              <?php echo $value; ?>
+            </li>
+          <?php endforeach; ?>
+        </ul><!--end of list-->
+      <?php endif; ?>
+    </div><!--end of container-->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
